d682dcb71d7cbde6d9be3cf99925a8720e25fa05
narishchi <naritsara.ma@kkumail.com>
2025-01-28 18:31:02 +0700

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace machineCoffe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# machineCoffee
+ 
